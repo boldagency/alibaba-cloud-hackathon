cae6583d1d11f1798bff9c5fa4ee240e481e7a9f
faisalbsh <94784597+faisalbsh@users.noreply.github.com>
2023-01-16 17:13:12 +0300

    .

---
+++ out/_next/static/a8iRfST3HUiz2p-zSIrmF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/236-a7eaa9a5c57a40ba.js","static/css/7869b1a360281c48.css","static/chunks/pages/index-fd8cf35e227e42e5.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
